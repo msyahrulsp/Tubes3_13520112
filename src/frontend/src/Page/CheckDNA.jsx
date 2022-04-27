7166ef22500b37911535f923cfe2769c13d6fae5
msyahrulsp <mssp892@gmail.com>
2022-04-27 09:22:47 +0700

    add checkdna page

--- src/frontend/src/Page/CheckDNA.jsx
+++ src/frontend/src/Page/CheckDNA.jsx
@@ -1,10 +1,30 @@
 import { React } from "react";
 import "./CheckDNA.scss";
 
+import { Template } from '../Templates/Template';
+import { Button } from '../Component/Button';
+
 export const CheckDNA = () => {
     return (
-        <div>
-            
-        </div>
+        <Template>
+            <div className="checkdna-container">
+                <div className="addna-title">
+                    <h3>Check DNA</h3>
+                </div>
+                <div className="checkdna-form">
+                    <div className="addna-form-nama">
+                    </div>
+                    <div className="addna-form-file">
+                    </div>
+                    <div className="addna-form-prediction">
+                    </div>
+                </div>
+                <div className="addna-button">
+                    <Button>
+                        <p>Submit</p>
+                    </Button>
+                </div>
+            </div>
+        </Template>
     )
 }
