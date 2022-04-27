2efe4217633c269dea02b4ea5a895a3633301a5a
msyahrulsp <mssp892@gmail.com>
2022-04-27 09:22:37 +0700

    add adddna page

--- src/frontend/src/Page/AddDNA.jsx
+++ src/frontend/src/Page/AddDNA.jsx
@@ -1,7 +1,28 @@
+import { React } from "react";
+import "./AddDNA.scss";
+
+import { Template } from '../Templates/Template';
+import { Button } from '../Component/Button';
+
 export const AddDNA = () => {
     return (
-        <div className="container">
-            <p>Hello World</p>
-        </div>
+        <Template>
+            <div className="adddna-container">
+                <div className="addna-title">
+                    <h3>Add DNA</h3>
+                </div>
+                <div className="addna-form">
+                    <div className="addna-form-nama">
+                    </div>
+                    <div className="addna-form-file">
+                    </div>
+                </div>
+                <div className="addna-button">
+                    <Button>
+                        <p>Submit</p>
+                    </Button>
+                </div>
+            </div>
+        </Template>
     )
 }
