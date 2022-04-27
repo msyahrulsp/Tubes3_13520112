7e91c4144e2dfa8175edf1f92d90ea39626632d9
msyahrulsp <mssp892@gmail.com>
2022-04-27 08:43:06 +0700

    add home page

--- src/frontend/src/Page/Home.jsx
+++ src/frontend/src/Page/Home.jsx
@@ -1,10 +1,20 @@
 import { React } from "react";
 import "./Home.scss";
 
+import { Template } from '../Templates/Template';
+
 export const Home = () => {
     return (
-        <div className="container">
-            <p>Hello World</p>
-        </div>
+        <Template>
+            <div className="home-container">
+                <div className="text-wrapper">
+                    <p>Penerapan String Matching dan</p>
+                    <p>Regular Expression dalam</p>
+                    <p>DNA Pattern Matching</p>
+                    <h1>Try it Now!</h1>
+                    <a href="/add-dna">Add DNA →</a>
+                </div>
+            </div>
+        </Template>
     )
 }
