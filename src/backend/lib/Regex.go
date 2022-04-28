765ef047999ff5f176a8fe37be6f602fbdd92466
fernaldyl <16520098@std.stei.itb.ac.id>
2022-04-28 12:30:04 +0700

    added output flag to regex

--- src/backend/lib/Regex.go
+++ src/backend/lib/Regex.go
@@ -9,7 +9,7 @@ func IsValidDNA(input string) bool {
 	return len(input) > 0 && !(regex.MatchString(input))
 }
 
-func IsValidQuery(input string) bool {
+func IsValidQuery(input string) int {
 
 	var regexDate31 *regexp.Regexp = regexp.MustCompile(`^(0?[1-9]|[12][1-9]|[123]0|31)[-/](0?[13578]|10|12)[-/]((202[0-2])|(20[01])[1-9]|(1[0-9]{1,3})|([0-9]{1,3}))$`)
 	var regexDate30 *regexp.Regexp = regexp.MustCompile(`^(0?[1-9]|[12][1-9]|[123]0)[-/](0?[469]|11)[-/]((202[0-2])|(20[01])[1-9]|(1[0-9]{1,3})|([0-9]{1,3}))$`)
@@ -17,12 +17,22 @@ func IsValidQuery(input string) bool {
 	var regexDisease *regexp.Regexp = regexp.MustCompile(`^[a-zA-Z0-9\\s]+$`)
 	var regexFull *regexp.Regexp = regexp.MustCompile(`^(((0?[1-9]|[12][1-9]|[123]0|31)[-/](0?[13578]|10|12)[-/]((202[0-2])|(20[01])[1-9]|(1[0-9]{1,3})|([0-9]{1,3})))|((0?[1-9]|[12][1-9]|[123]0)[-/](0?[469]|11)[-/]((202[0-2])|(20[01])[1-9]|(1[0-9]{1,3})|([0-9]{1,3})))|((0?[1-9]|[12][1-9]|[12]0)[-/](0?2)[-/]((202[0-2])|(20[01])[1-9]|(1[0-9]{1,3})|([0-9]{1,3}))))\\s[a-zA-Z0-9\\s]+$`)
 
+	var flag int = -1
+
 	// fmt.Println(regexDate31.FindAllString(input, -1))
 	// fmt.Println(regexDate30.FindAllString(input, -1))
 	// fmt.Println(regexDate29.FindAllString(input, -1))
 	// fmt.Println(regexDisease.FindAllString(input, -1))
 	// fmt.Println(regexFull.FindAllString(input, -1))
 
-	return regexDate31.MatchString(input) || regexDate30.MatchString(input) || regexDate29.MatchString(input) || regexDisease.MatchString(input) || regexFull.MatchString(input)
+	if regexDate31.MatchString(input) || regexDate30.MatchString(input) || regexDate29.MatchString(input) {
+		flag = 0 // Date
+	} else if regexDisease.MatchString(input) {
+		flag = 1 // Disease
+	} else if regexFull.MatchString(input) {
+		flag = 2 // Both
+	}
+
+	return flag
 
 }
